0c7b76dcf6128503c9bd6464bdfad686ef1833d3
Martin Durant <martin.durant@utoronto.ca>
2019-01-11 15:46:39 -0500

    tidy

--- intake_siphon/cat.py
+++ intake_siphon/cat.py
@@ -5,20 +5,24 @@ from intake.catalog.local import LocalCatalogEntry
 class SiphonCatalog(Catalog):
     name = 'thredds_cat'
 
-    def __init__(self, url, metadata=None, **kwargs):
+    def __init__(self, url, metadata=None):
         self.url = url
         super().__init__(metadata)
 
     def _load(self):
         from siphon.catalog import TDSCatalog
-        self.cat = TDSCatalog(self.url, **self.kwargs)
+        self.cat = TDSCatalog(self.url)
         self.name = self.cat.catalog_name
+
+        # sub-cats
         self._entries = {
             r.title: LocalCatalogEntry(
                 r.title, 'THREDDS cat', 'thredds_cat', True, {'url': r.href},
                 [], [], {}, None, catalog=self)
             for r in self.cat.catalog_refs.values()
         }
+
+        # data entries (only those with opendap links)
         self._entries.update({
             ds.name: LocalCatalogEntry(
                 ds.name, 'THREDDS data', 'netcdf', True,
@@ -26,10 +30,3 @@ class SiphonCatalog(Catalog):
                 [], [], {}, None, catalog=self)
             for ds in self.cat.datasets.values()
         })
-
-#(self, name, description, driver, direct_access, args, cache,
-#                 parameters, metadata, catalog_dir, getenv=True,
-#                 getshell=True, catalog=None)
-
-import intake
-intake.registry['thredds_cat'] = SiphonCatalog
