818127b1e8625704f5cf110913a3c5408d0f98c8
AS <aaron.spring@mpimet.mpg.de>
2021-02-12 19:57:46 +0100

    take smaller files for tests

--- tests/test_cat.py
+++ tests/test_cat.py
@@ -6,10 +6,12 @@ import xarray as xr
 
 @pytest.fixture(scope='module')
 def thredds_cat_url():
+    """Single file thredds catalog."""
     return 'https://psl.noaa.gov/thredds/catalog/Datasets/noaa.ersst/catalog.xml'
 
 
-def test_init_catalog(thredds_cat_url):
+def test_ThreddsCatalog_init_catalog(thredds_cat_url):
+    """Test initialization of ThreddsCatalog."""
     cat = intake.open_thredds_cat(thredds_cat_url)
     assert isinstance(cat, intake.catalog.Catalog)
     assert cat.metadata == cat.cat.metadata
@@ -22,16 +24,16 @@ def test_init_catalog(thredds_cat_url):
 
 
 @pytest.mark.parametrize('driver', ['netcdf', 'opendap'])
-def test_entry(thredds_cat_url, driver):
+def test_ThreddsCatalog(thredds_cat_url, driver):
     """Test entry.to_dask() is xr.Dataset and allows opendap and netcdf as source."""
     cat = intake.open_thredds_cat(thredds_cat_url, driver=driver)
-    entry = cat['err.mnmean.v3.nc']
+    entry = cat['sst.mon.19712000.ltm.v3.nc']
     if driver == 'opendap':
         assert isinstance(entry, intake_xarray.opendap.OpenDapSource)
     elif driver == 'netcdf':
         assert isinstance(entry, intake_xarray.netcdf.NetCDFSource)
     d = entry.describe()
-    assert d['name'] == 'err.mnmean.v3.nc'
+    assert d['name'] == 'sst.mon.19712000.ltm.v3.nc'
     assert d['container'] == 'xarray'
     assert d['plugin'] == [driver]
     if driver == 'opendap':
@@ -40,29 +42,32 @@ def test_entry(thredds_cat_url, driver):
         loc = 'fileServer'
     assert (
         d['args']['urlpath']
-        == f'https://psl.noaa.gov/thredds/{loc}/Datasets/noaa.ersst/err.mnmean.v3.nc'
+        == f'https://psl.noaa.gov/thredds/{loc}/Datasets/noaa.ersst/sst.mon.19712000.ltm.v3.nc'
     )
     ds = entry(chunks={}).to_dask()
     assert isinstance(ds, xr.Dataset)
 
 
-def test_entry_simplecache_netcdf(thredds_cat_url):
-    """Test allow simplecache:: in url if netcdf as source."""
+def test_ThreddsCatalog_simplecache_netcdf(thredds_cat_url):
+    """Test that ThreddsCatalog allows simplecache:: in url if netcdf as source."""
     import os
 
     import fsspec
 
     fsspec.config.conf['simplecache'] = {'cache_storage': 'my_caching_folder', 'same_names': True}
     cat = intake.open_thredds_cat(f'simplecache::{thredds_cat_url}', driver='netcdf')
-    entry = cat['err.mnmean.v3.nc']
+    entry = cat['sst.mon.19712000.ltm.v3.nc']
     ds = entry(chunks={}).to_dask()
     assert isinstance(ds, xr.Dataset)
     # test files present
-    os.path.exists('my_caching_folder/err.mnmean.v3.nc')
+    cached_file = 'my_caching_folder/sst.mon.19712000.ltm.v3.nc'
+    assert os.path.exists(cached_file)
+    os.remove(cached_file)
+    assert not os.path.exists(cached_file)
 
 
-def test_entry_simplecache_fails_opendap(thredds_cat_url):
-    """Test no simplecache:: in url with opendap."""
+def test_ThreddsCatalog_simplecache_fails_opendap(thredds_cat_url):
+    """Test that ThreddsCatalog simplecache:: in url with opendap."""
     with pytest.raises(ValueError) as e:
         intake.open_thredds_cat(f'simplecache::{thredds_cat_url}', driver='opendap')
     assert 'simplecache requires driver="netcdf"' in str(e.value)
