d4aefb90fb0c716e95b12c346237fdc5f8fb7365
Aaron Spring <aaronspring@users.noreply.github.com>
2021-05-06 00:47:16 +0200

    Update source.py

--- intake_thredds/source.py
+++ intake_thredds/source.py
@@ -87,7 +87,10 @@ class THREDDSMergedSource(DataSourceMixin):
                     break
             path = self.path[i:]
             if self.progressbar:
-                data = [ds.to_dask() for ds in tqdm(_match(cat, path), desc='Dataset(s)', ncols=79)]
+                if self.xarray_kwargs:
+                    data = [ds(**self.xarray_kwargs).to_dask() for ds in tqdm(_match(cat, path), desc='Dataset(s)', ncols=79)]
+                else:
+                    data = [ds.to_dask() for ds in tqdm(_match(cat, path), desc='Dataset(s)', ncols=79)]
             else:
                 data = [ds.to_dask() for ds in _match(cat, path)]
             self._ds = xr.combine_by_coords(data, combine_attrs='override')
