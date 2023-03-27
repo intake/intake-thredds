d49b76902bbd80288f4b2ee1c76014f30c89c537
Anderson Banihirwe <13301940+andersy005@users.noreply.github.com>
2023-03-27 12:03:48 -0600

    Update pre-commit & dependabot schedules (#118)
    * update pre-commit and dependabot schedules
    
    * formatting only

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,6 @@
-import intake_thredds
 import datetime
+
+import intake_thredds
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
