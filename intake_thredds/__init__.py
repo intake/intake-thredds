caf3c0cb63a3322141d87b5347b486e548910e33
Martin Durant <martin.durant@alumni.utoronto.ca>
2026-03-24 21:53:02 -0400

    hatch

--- intake_thredds/__init__.py
+++ intake_thredds/__init__.py
@@ -2,5 +2,8 @@
 
 import intake  # noqa: F401
 
+from ._version import __version__
 from .cat import ThreddsCatalog  # noqa: F401
 from .source import THREDDSMergedSource  # noqa: F401
+
+__all__ = ['ThreddsCatalog', 'THREDDSMergedSource', '__version__']
